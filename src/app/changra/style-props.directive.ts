e913a12d6c988de12a0635dbcddcecc38d115383
Joseph.Pfohl <joseph.pfohl@excellus.com>
2022-04-06 14:14:02 -0400

    initial commit

---
+++ src/app/changra/style-props.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, HostBinding, Inject, Input } from '@angular/core';
+import { ChangraSpacingKey, ChangraTheme, CHANGRA_THEME } from './changraTheme';
+
+@Directive({
+  selector: '[cuiStyled]',
+})
+export class ChangraStylePropsDirective {
+  constructor(@Inject(CHANGRA_THEME) private theme: ChangraTheme) {
+    console.log('IM BEING INITIALIZED');
+  }
+
+  // MARGIN
+
+  @HostBinding('style.margin') get hostMargin(): string {
+    const key = this.margin ?? this.m;
+
+    if (key) {
+      const val = this.theme.space[key];
+      if (val) return val;
+      else return `${key}`;
+    } else {
+      return undefined;
+    }
+  }
+
+  @Input() margin?: ChangraSpacingKey | string;
+  @Input() m?: ChangraSpacingKey | string;
+}
